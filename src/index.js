c82964935180fc36f3ed6ff9426de23862cfe980
Alexandr Mostovoy <ushanos@gmail.com>
2023-12-15 17:56:50 +0200

    wrapper compiled. COmmented some missing images in css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import { init} from "./init";
 
 const game = init();
+
+document.getElementById('loading_title').innerHTML = translate('LOADING');
+const bannerIMG = document.querySelector('#banner');
+bannerIMG.style.backgroundImage = `url(${config.skinData.banner})`
+bannerIMG.style.backgroundColor = config.skinData.bannerColor || '#911d23'
+
+const message = {
+  eventName: "all-loaded",
+};
+const hasParent = window.parent === window;
+window.parent.postMessage(message, '*');
