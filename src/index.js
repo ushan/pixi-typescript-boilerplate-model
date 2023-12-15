59a3ce1d9a782d66c93ce1ea45e83ef20f8b5976
Alexandr Mostovoy <ushanos@gmail.com>
2023-12-15 17:06:53 +0200

    game init in the  separate script

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-import * as PIXI from 'pixi.js';
-import { AppConfig } from './config/AppConfig';
-import Game from "./Game";
+import { init} from "./init";
 
-
-
-AppConfig.updateSize(window.innerWidth, window.innerHeight);
-const { gameWidth, gameHeight } = AppConfig.settings;
-
-const options = {
-    width: gameWidth,
-    height: gameHeight,
-    background: 0xDD2826,
-    antialias: true,
-    transparent: true,
-};
-const app = new PIXI.Application(options);
-
-window['globalThis']['__PIXI_APP__'] = app;
-
-const onResize = () => {
-    app.renderer.resize(window.innerWidth, window.innerHeight);
-    AppConfig.updateSize(window.innerWidth, window.innerHeight);
-}
-
-const init = () => {
-    window.addEventListener('resize', onResize);
-    const game = new Game(app);
-    app.stage.addChild(game);
-    onResize();
-};
-
-init();
-
-document.body.appendChild(app.view);
+const game = init();
---
+++ src/init.js
@@ -0,0 +1,40 @@
+import * as PIXI from 'pixi.js';
+import { AppConfig } from './config/AppConfig';
+import Game from "./Game";
+
+
+
+AppConfig.updateSize(window.innerWidth, window.innerHeight);
+const { gameWidth, gameHeight } = AppConfig.settings;
+
+const options = {
+    width: gameWidth,
+    height: gameHeight,
+    background: 0xDD2826,
+    antialias: true,
+    transparent: true,
+};
+const app = new PIXI.Application(options);
+
+window['globalThis']['__PIXI_APP__'] = app;
+
+const onResize = () => {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+    AppConfig.updateSize(window.innerWidth, window.innerHeight);
+}
+
+const init = () => {
+    window.addEventListener('resize', onResize);
+    const game = new Game(app);
+    app.stage.addChild(game);
+    onResize();
+    document.body.appendChild(app.view);
+    return game
+};
+
+init();
+
+
+
+
+export { EventBus, app, init };
